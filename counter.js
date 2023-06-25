bfd0238fba363ebdca2b2ff47d39098df2f2ee58
Dmitrii Kopasov <58292035+mandarin-coffee@users.noreply.github.com>
2023-06-25 12:34:23 +0300

    Test lint stage

---
+++ counter.js
@@ -0,0 +1,13 @@
+/**
+ * Функция vite
+ * @param {Object} element
+ */
+export function setupCounter(element) {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
---
+++ sum.js
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together.
+ * @param {int} a The first number.
+ * @param {int} b The second number.
+ * @return {int} The sum of the two numbers.
+ */
+export function sum(a, b) {
+  return a + b;
+}
